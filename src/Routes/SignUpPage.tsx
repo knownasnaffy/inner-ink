8cc85f724ec905523348e0028b16c1904582c26f
Naffy Dharni <naffydharni006@gmail.com>
2023-11-04 01:58:15 +0530

    style(auth): :lipstick: change and refactor style code in SignUpPage

--- src/Routes/SignUpPage.tsx
+++ src/Routes/SignUpPage.tsx
@@ -6,68 +6,7 @@ const SignUpPage = () => {
 			<div className='relative py-3 sm:max-w-xl sm:mx-auto'>
 				<div className='absolute inset-0 bg-gradient-to-r from-primary to-secondary shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl'></div>
 				<div className='relative shadow-lg sm:rounded-3xl'>
-					<div className='card max-w-md bg-base-200 mx-auto'>
-						<div className='card-body'>
-							<h1 className={clsx('card-title text-2xl w-full mb-2','table after:bg-gradient-to-r after:from-primary after:to-secondary after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full pb-1.5')}>
-								Register
-							</h1>
-							<form className=''>
-								<div className='flex gap-4'>
-									{/* First Name Field */}
-									<TextInput
-										label='First Name'
-										placeholder='John'
-									/>
-									{/* Last Name Field */}
-									<TextInput
-										label='Last Name'
-										placeholder='Doe'
-									/>
-								</div>
-								{/* Password Field */}
-								<div className='form-control w-full'>
-									<label className='label pt-px'>
-										<span className='label-text'>
-											Password
-										</span>
-									</label>
-									<input
-										type='text'
-										placeholder='********'
-										className='input input-bordered w-full focus:input-primary focus:focus-within:outline-offset-0 focus:border-none placeholder-neutral'
-									/>
-									<label className='label pb-px'>
-										<span className='label-text-alt invisible'>
-											Validation Error
-										</span>
-									</label>
-								</div>
-								{/* Repeat Password field */}
-								<div className='form-control w-full'>
-									<label className='label pt-px'>
-										<span className='label-text'>
-											Repeat Password
-										</span>
-									</label>
-									<input
-										type='text'
-										placeholder='********'
-										className='input input-bordered w-full focus:input-primary focus:focus-within:outline-offset-0 focus:border-none placeholder-neutral'
-									/>
-									<label className='label'>
-										<span className='label-text-alt invisible'>
-											Validation Error
-										</span>
-									</label>
-								</div>
-								<button
-									type='submit'
-									className='btn btn-primary btn-block'>
-									Continue
-								</button>
-							</form>
-						</div>
-					</div>
+					<FormCard />
 				</div>
 			</div>
 		</div>
@@ -78,26 +17,122 @@ export default SignUpPage
 
 interface TextInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 	label?: string
+	altLabel?: string
+	altLableClassName?: string
+	altLabelError?: boolean
+	altLabelWarn?: boolean
+	altLableSuccess?: boolean
 }
 
-const TextInput = ({ label, className, ...props }: TextInputProps) => {
+const TextInput = ({
+	label,
+	altLabel,
+	altLabelError,
+	altLabelWarn,
+	altLableSuccess,
+	className,
+	...props
+}: TextInputProps) => {
 	return (
 		<div className='form-control w-full'>
 			<label className='label'>
-				<span className='label-text'>{label}</span>
+				<span
+					className={clsx(
+						'label-text',
+						altLabelError
+							? 'text-error'
+							: altLabelWarn
+							? 'text-warning'
+							: altLableSuccess
+							? 'text-success'
+							: null
+					)}>
+					{label}
+				</span>
 			</label>
 			<input
 				className={clsx(
-					'input input-bordered hover:outline-none w-full focus:input-primary focus:focus-within:outline-offset-0 focus:border-none placeholder-neutral',
+					'input input-bordered w-full',
+					altLabelError
+						? 'input-error'
+						: altLabelWarn
+						? 'input-warning'
+						: altLableSuccess
+						? 'input-success'
+						: 'focus:input-primary',
+					'focus:focus-within:outline-offset-0 focus:border-none placeholder-neutral',
 					className
 				)}
 				{...props}
 			/>
-			<label className='label pb-px'>
-				<span className='label-text-alt invisible'>
-					Validation Error
+			<label className='label'>
+				<span
+					className={clsx(
+						'label-text-alt',
+						altLabelError
+							? 'text-error'
+							: altLabelWarn
+							? 'text-warning'
+							: altLableSuccess
+							? 'text-success'
+							: 'hidden'
+					)}>
+					{altLabel || 'ㅤ'}
 				</span>
 			</label>
 		</div>
 	)
 }
+
+const FormCard = () => (
+	<div className='card max-w-md bg-base-200 mx-auto'>
+		<div className='card-body'>
+			<h1
+				className={clsx(
+					'card-title text-2xl w-full mb-5',
+					'table after:bg-gradient-to-r after:from-primary after:to-secondary after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full pb-1.5'
+				)}>
+				Register
+			</h1>
+			<form className=''>
+				<div className='flex gap-4'>
+					{/* First Name Field */}
+					{/* IDEA: An option in settings to select honorifics(based on gender as well). For eg. Mr/Mrs FirstName, Mr/Mrs LastName, FirstName, LastName */}
+					<TextInput
+						label='First Name'
+						type='text'
+						placeholder='John'
+						altLabel='*First Name is required'
+						altLabelError
+					/>
+					{/* Last Name Field */}
+					<TextInput
+						label='Last Name'
+						type='text'
+						placeholder='Doe'
+					/>
+				</div>
+				{/* Password Field */}
+				{/* IDEA: An option in settings to only except numbers(pin) using attributes [inputmode="numeric"] and [minlength="4"] */}
+				<TextInput
+					label='Password'
+					type='password'
+					placeholder='********'
+					autoComplete='new-password'
+				/>
+				{/* Repeat Password field */}
+				<TextInput
+					label='Repeat Password'
+					type='password'
+					placeholder='********'
+					autoComplete='off'
+				/>
+				<button
+					type='submit'
+					className='btn btn-primary btn-block mt-2'>
+					Continue
+				</button>
+			</form>
+		</div>
+	</div>
+)
