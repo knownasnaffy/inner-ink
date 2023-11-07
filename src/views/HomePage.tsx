773fd9dd16fa9a3ec965d5c113083435fbf7483c
Naffy Dharni <naffydharni006@gmail.com>
2023-11-07 21:30:15 +0530

    feat(editor): add textarea as a placeholder for RTE

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,8 +1,15 @@
-import { redirect } from 'react-router-dom'
+import {
+	BookmarkSlashIcon,
+	EyeIcon,
+	Bars3Icon,
+} from '@heroicons/react/20/solid'
 import DateSelector from '../components/DateSelector'
 
 const HomePage = () => {
-	const openDateSelector = () => (document.getElementById('dateSelectorModal') as HTMLDialogElement).showModal()
+	const openDateSelector = () =>
+		(
+			document.getElementById('dateSelectorModal') as HTMLDialogElement
+		).showModal()
 
 	return (
 		<>
@@ -13,12 +20,23 @@ const HomePage = () => {
 					12 November 2344
 				</button>
 				<div className='flex flex-row w-fit h-fit'>
-					<button className='btn btn-ghost btn-square'>Oh</button>
-					<button className='btn btn-ghost btn-square'>Oh</button>
-					<button className='btn btn-ghost btn-square' onClick={() => redirect('/login')}>Oh</button>
+					<button className='btn btn-ghost btn-square'>
+						<BookmarkSlashIcon className='h-6' />
+					</button>
+					<button className='btn btn-ghost btn-square'>
+						<EyeIcon className='h-6' />
+					</button>
+					<button className='btn btn-ghost btn-square'>
+						<Bars3Icon className='h-6' />
+					</button>
 				</div>
 			</div>
-			<div className='grow'></div>
+			<div className='grow'>
+				{/* TODO: Manage textarea height */}
+				<textarea
+					className='textarea resize-none box-border h-full w-full bg-base-200'
+					placeholder='How was your day?'></textarea>
+			</div>
 			<DateSelector />
 		</>
 	)
