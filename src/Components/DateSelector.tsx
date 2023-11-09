149f266850ded1dd6210a217e7c76748e4020ba1
Naffy Dharni <naffydharni006@gmail.com>
2023-11-09 23:16:02 +0530

    style: :lipstick: change date picker styles
    Some style changes and a function that if a day outside of month is selected, the shown month also changes. Removed stale code & commits. Add new info comits. Note: There was a problem with date-picker component height as it sometimes displayed an additional week in some months, but it got fixed by itself.

--- src/Components/DateSelector.tsx
+++ src/Components/DateSelector.tsx
@@ -1,75 +1,73 @@
 import { ClassNames, DayClickEventHandler, DayPicker } from 'react-day-picker'
-// import 'react-day-picker/dist/style.css'
 
-import { format } from 'date-fns'
 import dateStore from '../hooks/dateStore'
 import appSettings from '../utils/appSettings'
 
-// import styles from 'react-day-picker/dist/style.module.css';
-
 const bookmarkedDays = [new Date(2023, 10, 1), new Date(2023, 10, 4)]
-// const bookmarkStyle = { border: '2px solid currentColor' }
 
 const DateSelector = () => {
 	const closeDateSelector = () =>
 		(
 			document.getElementById('dateSelectorModal') as HTMLDialogElement
 		).close()
+
 	const selectedDay = dateStore((state) => state.selectedDay)
 	const setSelectedDay = dateStore((state) => state.setSelectedDay)
+	const visibleMonth = dateStore((state) => state.visibleMonth)
+	const setVisibleMonth = dateStore((state) => state.setVisibleMonth)
+
 	const handleDayClick: DayClickEventHandler = (day) => {
 		setSelectedDay(day)
-		closeDateSelector()
-	}
-	let footer = <p>Please pick a day.</p>
-	if (selectedDay) {
-		footer = <p>You picked {format(selectedDay, 'PP')}.</p>
+		setVisibleMonth(day)
+		setTimeout(() => {
+			closeDateSelector()
+		}, 500)
 	}
 
 	const classNames: ClassNames = {
-    // ...styles,
-    // head: 'custom-head'
-		vhidden: 'hidden',
 		button: 'dp-btn',
 		caption: 'dp-caption',
 		caption_label: 'dp-caption-label',
 		nav: 'dp-nav',
-		nav_button: 'dp-nav-button',
-		table: 'dp-tabel',
-  };
-	
+		nav_button: 'dp-nav-btn',
+		table: 'dp-table',
+	}
+
 	const datePickerSettings = appSettings.datePicker
 	return (
-		<dialog id='dateSelectorModal' className='modal'>
-			<div className='modal-box max-w-fit px-8 h-[28rem] flex items-center'>
+		<dialog id='dateSelectorModal' className='modal align-top'>
+			<div className='modal-box max-w-fit flex flex-col justify-between'>
 				<DayPicker
 					id='datePicker'
-					mode='single'
+					mode='single' // Allow only a single day to be selected
 					selected={selectedDay}
-					onDayClick={handleDayClick}
-					// footer={footer} // Footer displayed below calendar
-					toDate={datePickerSettings.disableFutureEntry ? new Date() : undefined} // Disable future selection
-					// showOutsideDays // Display days of next and previous months
+					onDayClick={handleDayClick} // Perform other functions as well when day is selected
+					toDate={
+						datePickerSettings.disableFutureEntry
+							? new Date()
+							: undefined
+					} // Disable future selection
+					month={visibleMonth}
+					onMonthChange={setVisibleMonth} // Month displayed changes when an outside day is selected
+					showOutsideDays // Display days of next and previous months
 					fixedWeeks // Display 6 weeks at a time
 					weekStartsOn={datePickerSettings.weekStart} // Start of the week
-					modifiers={{ booked: bookmarkedDays }}
-          modifiersClassNames={{
-            selected: 'dp-selected',
-            today: 'dp-today',
-            bookmarked: 'dp-bookmarked',
-            outside: 'outside-day',
-            disabled: 'disabled-day',
-          }}
+					modifiers={{ bookmarked: bookmarkedDays }}
+					modifiersClassNames={{
+						selected: 'dp-selected-day',
+						today: 'dp-today',
+						bookmarked: 'dp-bookmarked',
+						outside: 'dp-outside-day',
+						disabled: 'disabled-day',
+					}}
 					classNames={classNames}
 				/>
-				{/* <h3 className="font-bold text-lg">Hello!</h3>
-    <p className="py-4">Press ESC key or click outside to close</p> */}
-				{/* <div className="modal-action"> */}
-				{/* <form method="dialog"> */}
-				{/* if there is a button in form, it will close the modal */}
-				{/* <button className="btn">Close</button>
-      </form> */}
-				{/* </div> */}
+				<div className='modal-action'>
+					<form method='dialog'>
+						{/* if there is a button in form, it will close the modal */}
+						<button className='btn'>Cancel</button>
+					</form>
+				</div>
 			</div>
 			<form method='dialog' className='modal-backdrop'>
 				<button>close</button>
--- src/hooks/dateStore.ts
+++ src/hooks/dateStore.ts
@@ -3,11 +3,15 @@ import { create } from 'zustand'
 type DateStoreState = {
 	selectedDay: Date
 	setSelectedDay: (day: Date) => void
+	visibleMonth: Date
+	setVisibleMonth: (month: Date) => void
 }
 
 const dateStore = create<DateStoreState>((set) => ({
 	selectedDay: new Date(),
-	setSelectedDay: (day) => set(() => ({selectedDay: day }))
+	setSelectedDay: (day) => set(() => ({ selectedDay: day })),
+	visibleMonth: new Date(),
+	setVisibleMonth: (month) => set(() => ({ visibleMonth: month })),
 }))
 
 export default dateStore
