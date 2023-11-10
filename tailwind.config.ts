6eaf42ef79f4cc87dc7f7fd6d66e312407585e03
Naffy Dharni <naffydharni006@gmail.com>
2023-11-10 12:16:47 +0530

    style: :heavy_plus_sign: add `@tailwindcss/typography' for text styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,7 @@ export default {
 	theme: {
 		extend: {},
 	},
-	plugins: [require('daisyui')],
+	plugins: [require('@tailwindcss/typography'), require('daisyui')],
 	daisyui: {
 		themes: true,
 	},
