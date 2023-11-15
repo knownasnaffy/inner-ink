595d63cadcad924d5d38ad433b1d5b21bbef7275
Naffy Dharni <naffydharni006@gmail.com>
2023-11-16 01:01:12 +0530

    feat: :sparkles: add splashscreen

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { invoke } from '@tauri-apps/api/tauri'
 
 import App from './App'
 
 import './index.css'
+
+document.addEventListener('DOMContentLoaded', () => {
+	// This will wait for the window to load, but you could
+	// run this function on whatever trigger you want
+	invoke('close_splashscreen')
+})
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
