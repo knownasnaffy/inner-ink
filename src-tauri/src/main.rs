38adf5601769927f4c07f3443bc3eb5c8fa51e22
Naffy Dharni <naffydharni006@gmail.com>
2023-11-16 00:01:42 +0530

    feat: :construction: (WIP) add custom title bar

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,16 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+use window_shadows::set_shadow;
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+            set_shadow(&window, true).expect("Unsupported platform!");
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
