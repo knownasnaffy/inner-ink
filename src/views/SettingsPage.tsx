177112a229db541cd595fc01a3cfe712eba67701
Naffy Dharni <naffydharni006@gmail.com>
2023-11-19 00:03:28 +0530

    feat: add version section in settings

--- src/views/SettingsPage.tsx
+++ src/views/SettingsPage.tsx
@@ -1,13 +1,52 @@
 const SettingsPage = () => {
 	return (
-		<div className='h-full w-full'>
-			<h2 className='text-2xl font-semibold mb-4 table pb-1 after:bg-base-300 after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-full'>
-				Preferences
+		<>
+			<h2 className='text-2xl font-semibold mb-4 table rounded-none pb-1 after:bg-base-300 after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-full sticky top-0 z-50 bg-base-100'>
+				Settings
 			</h2>
-			<h3 className='text-lg font-semibold mb-2'>
+			<h3 className='text-lg font-semibold mb-2'>Version</h3>
+			<div className='collapse-title bg-base-200 text-md card rounded-b-none flex flex-row justify-between items-center pr-4'>
+				<div className='flex flex-row items-center gap-2 font-semibold'>
+					<svg
+						xmlns='http://www.w3.org/2000/svg'
+						fill='none'
+						viewBox='0 0 24 24'
+						strokeWidth={1.5}
+						stroke='currentColor'
+						className='w-6 h-6 shrink-0'
+					>
+						<path
+							strokeLinecap='round'
+							strokeLinejoin='round'
+							d='M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99'
+						/>
+					</svg>
+					<div>
+						v0.1.0
+						<p className='text-sm text-base-content/50'>
+							Last checked: 18-11-2023
+						</p>
+					</div>
+				</div>
+				<button className='btn btn-primary btn-sm capitalize'>
+					Check for updates
+				</button>
+			</div>
+			<div className='collapse-title bg-base-200 text-md card rounded-t-none border-t-2 flex flex-row justify-between items-center pr-4'>
+				<div className='flex flex-row items-center gap-2 font-semibold'>
+					<div>
+						Download updates automatically
+						<p className='text-sm text-base-content/50'>
+							Except on metered connections
+						</p>
+					</div>
+				</div>
+				<input type='checkbox' className='toggle toggle-primary' />
+			</div>
+			<h3 className='text-lg font-semibold mt-4 mb-2'>
 				Appearance & Behaviour
 			</h3>
-			<div className='join join-vertical w-full'>
+			<div>
 				<div className='collapse collapse-arrow bg-base-200 rounded-b-none'>
 					<input type='checkbox' />
 					<div className='collapse-title text-md font-semibold'>
@@ -113,7 +152,7 @@ const SettingsPage = () => {
 								d='M10.5 21l5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 016-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 01-3.827-5.802'
 							/>
 						</svg>
-						<div className=''>
+						<div>
 							Language
 							<p className='text-sm text-base-content/50'>
 								Select a display language fpr this app. Overides
@@ -121,14 +160,17 @@ const SettingsPage = () => {
 							</p>
 						</div>
 					</div>
-					<select className='select select-sm select-bordered w-full max-w-fit'>
-						<option selected>System Default</option>
+					<select
+						className='select select-sm select-bordered w-full max-w-fit'
+						defaultValue='System Default'
+					>
+						<option>System Default</option>
 						<option>English</option>
 						<option>French</option>
 					</select>
 				</div>
 			</div>
-		</div>
+		</>
 	)
 }
 
