2f781cbdd218a936ec24d934b887b52a8444f572
Naffy Dharni <naffydharni006@gmail.com>
2023-11-18 16:58:25 +0530

    feat: :sparkles: make layout corner theme responsive

--- src/views/AppLayout.tsx
+++ src/views/AppLayout.tsx
@@ -4,7 +4,7 @@ const AppLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div className='grow flex flex-row bg-base-300'>
 			<Sidebar />
-			<div className='grow py-5 px-8 md:px-10 lg:px-12 bg-base-100 rounded-tl-md'>
+			<div className='grow py-5 px-8 md:px-10 lg:px-12 bg-base-100 rounded-tl-[var(--rounded-box)]'>
 				{children}
 			</div>
 		</div>
