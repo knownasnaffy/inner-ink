ccbc79c47bc5cc7d6588897c2031e92fd1edcdd9
Naffy Dharni <naffydharni006@gmail.com>
2023-11-19 18:08:05 +0530

    style: :lipstick: improve system theme button style

--- src/Components/ThemeList/index.tsx
+++ src/Components/ThemeList/index.tsx
@@ -10,15 +10,30 @@ export const ThemeList = () => {
 		<>
 			<div
 				className={clsx(
-					'bg-base-100 p-2 card gap-1',
-					theme === '' && 'outline-dashed outline-offset-1',
+					'relative',
+					theme === '' &&
+						'rounded-box outline-dashed outline-offset-1',
 				)}
 				onClick={() => setTheme('')}
 			>
-				<span className='bg-primary card p-1.5'></span>
-				<span className='bg-secondary card p-1.5 w-3/4'></span>
-				<span className='bg-accent card p-1.5 w-1/2'></span>
-				<p className='text-sm'>System Default</p>
+				<div
+					className='absolute top-0 left-0 flex flex-col gap-1 rounded-box py-2 pl-2 w-full clip-path-left'
+					data-theme='light'
+				>
+					<span className='bg-primary rounded-l-box p-1.5'></span>
+					<span className='bg-secondary rounded-l-box p-1.5 w-3/4'></span>
+					<span className='bg-accent rounded-l-box p-1.5 w-1/2'></span>
+					<p className='text-sm'>System Default</p>
+				</div>
+				<div
+					className='absolute top-0 left-0 flex flex-col gap-1 rounded-box py-2 pr-2 w-full clip-path-right'
+					data-theme='dark'
+				>
+					<span className='bg-primary card p-1.5 w-full'></span>
+					<span className='bg-secondary card p-1.5 w-full'></span>
+					<span className='bg-accent card p-1.5 w-full'></span>
+					<p className='text-sm'>System Default</p>
+				</div>
 			</div>
 			{themes.map((theme, index) => {
 				return <ThemeButton key={index} themeName={theme} />
