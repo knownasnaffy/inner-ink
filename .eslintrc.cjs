4268b066bb0972c7ff14a6d83b2cd80e58b8b74b
Naffy Dharni <naffydharni006@gmail.com>
2023-11-20 22:35:07 +0530

    chore: :wrench: config eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,17 @@ module.exports = {
 	env: { browser: true, es2020: true },
 	extends: [
 		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended-type-checked',
-		'plugin:react-hooks/recommended',
-		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
 		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
 		'prettier',
 	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: ['./tsconfig.json', './tsconfig.node.json'],
+		tsconfigRootDir: __dirname,
+	},
 	ignorePatterns: ['dist', '.eslintrc.cjs'],
 	parser: '@typescript-eslint/parser',
 	plugins: ['react-refresh'],
@@ -17,6 +22,7 @@ module.exports = {
 			'warn',
 			{ allowConstantExport: true },
 		],
+		'react/react-in-jsx-scope': 0,
 		'@typescript-eslint/no-explicit-any': 'warn',
 	},
 }
