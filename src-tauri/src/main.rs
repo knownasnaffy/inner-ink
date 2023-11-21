8f54f1414901fbade6201f970b15592bc9db86a3
Naffy Dharni <naffydharni006@gmail.com>
2023-11-21 18:06:29 +0530

    feat: :sparkles: ensure only a single instance is running at a time

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,6 +4,12 @@
 use tauri::Manager;
 use window_shadows::set_shadow;
 
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+    args: Vec<String>,
+    cwd: String,
+}
+
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
@@ -11,6 +17,12 @@ fn main() {
             set_shadow(&main_window, true).expect("Unsupported platform!");
             Ok(())
         })
+        .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
+            println!("{}, {argv:?}, {cwd}", app.package_info().name);
+
+            app.emit_all("single-instance", Payload { args: argv, cwd })
+                .unwrap();
+        }))
         .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_window_state::Builder::default().build())
         .run(tauri::generate_context!())
