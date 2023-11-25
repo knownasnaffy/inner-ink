12ba1de9e744355231756c9155d95c6c86184bfc
Naffy Dharni <naffydharni006@gmail.com>
2023-11-25 16:25:15 +0530

    feat: :sparkles: add date picker settings placeholder on SettingsPage

--- src/views/SettingsPage.tsx
+++ src/views/SettingsPage.tsx
@@ -140,6 +140,41 @@ const SettingsPage = () => {
 					<option>French</option>
 				</select>
 			</div>
+			{/* ## Date picker */}
+			<h3 className='mt-4 mb-2 text-lg font-semibold'>Date Picker</h3>
+			{/* ### Disable Entry in Future */}
+			<div className='flex flex-row items-center justify-between pr-4 border-t-2 border-base-100 collapse-title bg-base-200 text-md rounded-t-btn'>
+				<div className='flex flex-row items-center gap-2 font-semibold'>
+					<div className=''>
+						Disable entry in future
+						<p className='text-sm text-base-content/50'>
+							You will not be able to write an entry for a date in
+							future
+						</p>
+					</div>
+				</div>
+				<input type='checkbox' className='toggle toggle-primary' />
+			</div>
+			{/* ### First day of the week */}
+			<div className='flex flex-row items-center justify-between gap-4 pr-4 border-t-2 border-base-100 rounded-b-btn collapse-title bg-base-200 text-md'>
+				<div className='flex flex-row items-center gap-2 font-semibold'>
+					<div>
+						First day of the week
+						{/* <p className='text-sm text-base-content/50'>
+							Select a display language fpr this app. Overides
+							default Windows interface language
+						</p> */}
+					</div>
+				</div>
+				<select
+					className='w-full select select-bordered select-sm max-w-fit'
+					defaultValue='System Default'
+				>
+					<option>System Default</option>
+					<option>English</option>
+					<option>French</option>
+				</select>
+			</div>
 		</div>
 	)
 }
