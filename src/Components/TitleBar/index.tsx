385d49a270b0f648a41539457447ac244b883f18
Naffy Dharni <naffydharni006@gmail.com>
2023-11-26 02:44:32 +0530

    fix: :bug: app name container was not a draggable region

--- src/Components/TitleBar/index.tsx
+++ src/Components/TitleBar/index.tsx
@@ -4,9 +4,9 @@ const TitleBar = () => {
 	return (
 		<div
 			data-tauri-drag-region
-			className='h-8 pl-4 pr-2 select-none bg-base-300 flex flex-row justify-between items-center'
+			className='h-8 pl-4 pr-2 bg-base-300 flex flex-row justify-between items-center'
 		>
-			<span className='flex items-center text-base-content/60'>
+			<span className='flex items-center text-base-content/60 pointer-events-none'>
 				Inner Ink
 				<span className='ml-2 badge badge-sm badge-secondary'>
 					Preview
