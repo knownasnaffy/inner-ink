2eea1678693e5475c0a3f3bdc413e528f88ac320
Naffy Dharni <naffydharni006@gmail.com>
2023-12-01 03:08:48 +0530

    feat: :construction: animate search results (WIP)

--- src/Components/Search/SearchResults.tsx
+++ src/Components/Search/SearchResults.tsx
@@ -1,6 +1,6 @@
 export const SearchResult = () => {
 	return (
-		<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+		<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 animate-in slide-in-from-bottom duration-300'>
 			<div className='bg-base-200 p-4'></div>
 		</div>
 	)
@@ -8,7 +8,7 @@ export const SearchResult = () => {
 
 export const ResultNotFound = () => {
 	return (
-		<div className='flex flex-col text-center justify-center grow mt-4 fallback-response bg-base-200 rounded-box text-base-content/70'>
+		<div className='flex flex-col text-center justify-center grow mt-4 fallback-response bg-base-200 rounded-box text-base-content/70 animate-in slide-in-from-bottom duration-300'>
 			<p className='text-4xl font-extrabold'>＞﹏＜</p>
 			<p className='text-2xl px-4'>
 				No matching results. Did you ever write this?
@@ -19,7 +19,7 @@ export const ResultNotFound = () => {
 
 export const EmptySearchResult = () => {
 	return (
-		<div className='flex flex-col text-center justify-center grow mt-4 fallback-response bg-base-200 rounded-box text-base-content/70'>
+		<div className='flex flex-col text-center justify-center grow mt-4 fallback-response bg-base-200 rounded-box text-base-content/70 animate-in slide-in-from-bottom duration-300'>
 			<p className='text-4xl font-extrabold'>( ﾉ ﾟｰﾟ)ﾉ</p>
 			<p className='text-2xl px-4'>
 				Nothing here. Maybe type something in that field up there?
