27988dc9da8db079cdb4e61051d952f2377e47df
Naffy Dharni <naffydharni006@gmail.com>
2023-12-01 15:23:34 +0530

    feat: :sparkles: add custom caption component for day-picker

--- src/Components/DateSelector.tsx
+++ src/Components/DateSelector.tsx
@@ -1,10 +1,70 @@
-import { ClassNames, DayClickEventHandler, DayPicker } from 'react-day-picker'
+import {
+	CaptionProps,
+	ClassNames,
+	DayClickEventHandler,
+	DayPicker,
+	useNavigation,
+} from 'react-day-picker'
 import { useHotkeys } from 'react-hotkeys-hook'
 
 import dateStore from '../hooks/dateStore'
 import { getSettings } from '../utils/settings'
+import { format } from 'date-fns'
 
 const bookmarkedDays = [new Date(2023, 10, 1), new Date(2023, 10, 4)]
+
+function CustomCaption(props: CaptionProps) {
+	const { goToMonth, nextMonth, previousMonth } = useNavigation()
+	return (
+		<div className='text-primary flex justify-between items-center pb-2'>
+			<p className='font-bold text-2xl pl-2.5'>
+				{format(props.displayMonth, 'MMMM yyyy')}
+			</p>
+			<div className='w-fit flex gap-1'>
+				<button
+					className='btn btn-circle btn-ghost'
+					disabled={!previousMonth}
+					onClick={() => previousMonth && goToMonth(previousMonth)}
+				>
+					<svg
+						xmlns='http://www.w3.org/2000/svg'
+						fill='none'
+						viewBox='0 0 24 24'
+						strokeWidth={3}
+						stroke='currentColor'
+						className='w-6 h-6'
+					>
+						<path
+							strokeLinecap='round'
+							strokeLinejoin='round'
+							d='M15.75 19.5L8.25 12l7.5-7.5'
+						/>
+					</svg>
+				</button>
+				<button
+					className='btn btn-circle btn-ghost'
+					disabled={!nextMonth}
+					onClick={() => nextMonth && goToMonth(nextMonth)}
+				>
+					<svg
+						xmlns='http://www.w3.org/2000/svg'
+						fill='none'
+						viewBox='0 0 24 24'
+						strokeWidth={3}
+						stroke='currentColor'
+						className='w-6 h-6'
+					>
+						<path
+							strokeLinecap='round'
+							strokeLinejoin='round'
+							d='M8.25 4.5l7.5 7.5-7.5 7.5'
+						/>
+					</svg>
+				</button>
+			</div>
+		</div>
+	)
+}
 
 const DateSelector = () => {
 	const closeDateSelector = () =>
@@ -64,9 +124,12 @@ const DateSelector = () => {
 						today: 'dp-today',
 						bookmarked: 'dp-bookmarked',
 						outside: 'dp-outside-day',
-						disabled: 'disabled-day',
+						disabled: 'dp-disabled-day',
 					}}
 					classNames={classNames}
+					components={{
+						Caption: CustomCaption,
+					}}
 				/>
 				<div className='modal-action'>
 					<form method='dialog'>
