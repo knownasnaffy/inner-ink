aa6f0a56d7728db193730be3a57070ade90239d5
Naffy Dharni <naffydharni006@gmail.com>
2023-12-08 01:33:16 +0530

    refactor: :truck: change to monorepo structure and move app code to `./app`

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-	content: ['./index.html', './src/frontend/**/*.{js,ts,jsx,tsx}'],
+	content: ['./frontend/**/*.{js,ts,jsx,tsx,html}'],
 	theme: {
 		extend: {},
 	},
