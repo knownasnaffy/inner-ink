9d336ee87d0b881e71bd842ec7e7aaf626c065ce
Naffy Dharni <naffydharni006@gmail.com>
2023-12-25 10:54:07 +0530

    fix: :bug: application window can now be resized from titlebar edges

--- app/frontend/components/TitleBar/index.tsx
+++ app/frontend/components/TitleBar/index.tsx
@@ -2,17 +2,19 @@ import { ShellButtons } from './ShellButtons'
 
 const TitleBar = () => {
 	return (
-		<div
-			data-tauri-drag-region
-			className='h-8 pl-4 pr-2 bg-base-300 flex flex-row justify-between items-center'
-		>
-			<span className='flex items-center text-base-content/60 pointer-events-none'>
-				Inner Ink
-				<span className='ml-2 badge badge-sm badge-secondary'>
-					Preview
+		<div className='h-fit py-1 bg-base-300'>
+			<div
+				data-tauri-drag-region
+				className='h-6 ml-1 pl-3 mr-1 pr-1 flex flex-row justify-between items-center'
+			>
+				<span className='flex items-center text-base-content/60 pointer-events-none'>
+					Inner Ink
+					<span className='ml-2 badge badge-sm badge-secondary'>
+						Preview
+					</span>
 				</span>
-			</span>
-			<ShellButtons />
+				<ShellButtons />
+			</div>
 		</div>
 	)
 }
