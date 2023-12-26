8f2a1682185b85459100f5a94574a19f00e96ccf
Naffy Dharni <naffydharni006@gmail.com>
2023-12-26 11:31:53 +0530

    fix: :ambulance: include initDB function in main script

--- app/frontend/main.tsx
+++ app/frontend/main.tsx
@@ -2,8 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import RouterComponent from './Router'
+import { initDB } from './utils/database'
 
 import './index.css'
+
+initDB()
 
 createRoot(document.querySelector('#root')!).render(
 	<StrictMode>
