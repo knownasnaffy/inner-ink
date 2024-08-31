77b42f1c2a6870744aadd5893a4d3ba3f7602264
Naffy Dharni <naffydharni006@gmail.com>
2024-08-31 19:58:06 +0530

    add boilerplate code

--- desktop/frontend/App.tsx
+++ desktop/frontend/App.tsx
@@ -1,11 +1,30 @@
+import { useState } from 'react'
 import TitleBar from './components/Global/TitleBar'
+import { Calendar } from '@/components/ui/calendar'
+import { Input } from './components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
 
 function App() {
+	const [date, setDate] = useState<Date | undefined>(new Date())
 	return (
 		<>
 			<div className='flex flex-col w-full h-screen'>
 				<TitleBar />
-				<div className='grow p-4'>This works! {'>_<'}</div>
+				<div className='grow p-4 flex'>
+					<div className='w-fit px-10 flex flex-col justify-center gap-4'>
+						<Input />
+						<Calendar
+							mode='single'
+							selected={date}
+							onSelect={setDate}
+							toDate={new Date()}
+							className='rounded-md border'
+						/>
+					</div>
+					<div className='p-10 w-full'>
+						<Textarea className='h-full resize-none' />
+					</div>
+				</div>
 			</div>
 		</>
 	)
